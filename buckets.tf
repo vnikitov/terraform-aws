fede92b0e736c99ada64cdb88a9448d125f6107c
Vladisalv Trayanov <vladislav@tradeo.com>
2018-09-11 18:36:41 +0300

    Fix S3 upload script

---
+++ buckets.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "files-bucket" {
+  bucket = "s3-files-vlad"
+  acl    = "private"
+  force_destroy = false
+  
+  versioning {
+    enabled = false
+  }
+  
+  tags {
+    Environment = "Dev"
+  }
+}
