025e3c7ec82ddaa452db41a9a05032ed714317e6
Vladisalv Trayanov <vladislav@tradeo.com>
2018-09-13 11:58:14 +0300

    Fix upload script

--- s3-upload.py
+++ s3-upload.py
@@ -4,13 +4,12 @@ import os
 import sys
 
 os.chdir('/home/vladislav/s3-upload')
-#local_dir = ('/home/vladislav/s3-upload/')
 bucketName = "s3-files-vlad"
 #files = glob.glob('/home/vladislav/s3-upload/**/*', recursive=True)
-#print (files), 'will be uploaded to S3'
 
 s3 = boto3.client('s3')
 
 for dirpath, dirs, files in os.walk('/home/vladislav/s3-upload/'):
   for fname in files:
     s3.upload_file(dirpath+ '/' + fname, bucketName, fname)
+    print (fname, 'will be uploaded')
