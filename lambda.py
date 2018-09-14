ea74641284ef6803786c4c6e56245baaa00a730d
Vladisalv Trayanov <vladislav@tradeo.com>
2018-09-14 16:17:06 +0300

    Fix lambda

--- lambda.py
+++ lambda.py
@@ -6,7 +6,7 @@ get_last_modified = lambda obj: int(obj['LastModified'].strftime('%s'))
 s3 = boto3.client('s3')
 objs = s3.list_objects_v2(Bucket='s3-files-vlad')['Contents']
 [obj['Key'] for obj in sorted(objs, key=get_last_modified)]
-print (objs[0])
+#print (objs[0])
 
 def execute_commands_on_linux_instances(client, commands, instance_ids):
     resp = client.send_command(
@@ -18,6 +18,6 @@ def execute_commands_on_linux_instances(client, commands, instance_ids):
 
 ssm_client = boto3.client('ssm')
 commands = [f'echo "{objs}" was uploaded to S3 >> /var/log/custom_log_file']
-print (commands[0])
+#print (commands[0])
 instance_ids = ['i-0185b7f444a35e008']
 execute_commands_on_linux_instances(ssm_client, commands, instance_ids)
